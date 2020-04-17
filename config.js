7c77eda7db3c59ea1b5078db05f6e63443389e62
cjh0613 <cjhbruce@foxmail.com>
2020-04-17 16:58:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,46 +4,6 @@ module.exports = {
       'key': 'google',
       'proxied': 'https://www.google.com/',
       'proxying': ''
-    },
-    {
-      'key': 'scholar.google',
-      'proxied': 'https://scholar.google.com/',
-      'proxying': ''
-    },
-    {
-      'key': 'maps.google',
-      'proxied': 'https://maps.google.com/',
-      'proxying': ''
-    },
-    {
-      'key': 'translate.google',
-      'proxied': 'https://translate.google.com/',
-      'proxying': ''
-    },
-    {
-      'key': 'docs.google',
-      'proxied': 'https://docs.google.com/',
-      'proxying': ''
-    },
-    {
-      'key': 'drive.google',
-      'proxied': 'https://drive.google.com/',
-      'proxying': ''
-    },
-    {
-      'key': 'code.google',
-      'proxied': 'https://code.google.com/',
-      'proxying': ''
-    },
-    {
-      'key': 'wiki',
-      'proxied': 'https://zh.wikipedia.org/',
-      'proxying': ''
-    },
-    {
-      'key': 'mwiki',
-      'proxied': 'https://zh.m.wikipedia.org/',
-      'proxying': ''
     }
   ]
 }
